737e3dea534c5958ceec278381a834c1e717467a
Alan Krajina <alankrajina88@gmail.com>
2019-10-14 17:11:46 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ class App extends Component {
 	render() {
 		// your code in the return statement below!
 		return (
-			`<div className="App">
-				<CatComponent />
-				<EinsteinQuoteComponent />
-				<MouseComponent />
-			</div>`
+			<div className="App">
+				`<CatComponent />`
+				`<EinsteinQuoteComponent />`
+				`<MouseComponent />`
+			</div>
 		);
 	}
 }
